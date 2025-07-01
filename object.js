263fe8f03da293525fb747846c58abb15ed3c03e
Adhish <adhishullasan@gmail.com>
2025-07-01 16:37:01 +0530

    complete object

--- object.js
+++ object.js
@@ -1,6 +1,11 @@
 //Program to add a property to an object
 
-const obj = { name: 'Henry'}
-obj.age = 20
-obj.grnder = 'Male' 
+const obj = { name: 'Henry'}    
+obj.age = 20                       
+obj.gender = 'Male'                 
+console.log(obj)     
+
+//Program to delete a property from an object
+
+delete obj.gender
 console.log(obj)
