56a4c599ef6f1d571a25dd47c384c046564e4cbe
Adhish <adhishullasan@gmail.com>
2025-07-18 22:06:50 +0530

    javascript:sum of array elements

---
+++ sumofArray.js
@@ -0,0 +1,21 @@
+function sumArray(arr) {
+
+    let sum = 0
+    for (let i = 0; i < arr.length; i++) {
+
+        sum += arr[i]
+    }
+    return sum
+}
+
+console.log(sumArray([40,5,3,11]))
+
+// function sumArray(arr) {
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         sum += arr[i];
+//     }
+//     return sum;
+// }
+
+// console.log(sumArray([15, 6, 10, 2]));
